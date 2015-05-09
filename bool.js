fd5076cd51a784c8ff999653f7e8dda32200959f
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-05-09 12:52:42 -0600

    added more evaluation blocks

--- bool.js
+++ bool.js
@@ -11,9 +11,14 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    function conv(val) { return (val == "true") || (val == "1"); }
+
     ext.t = function() { return true; }
     ext.f = function() { return false; }
-    ext.evaluate = function(val) { return (val == "true"); }
+    ext.evaluate = function(val) { return conv(val); }
+    ext.evalnot = function(val) { return !conv(val); }
+    ext.evaland = function(val1, val2) { return conv(val1) && conv(val2); }
+    ext.evalor = function(val1, val2) { return conv(val1) || conv(val2); }
 
     // Block and block menu descriptions
     var descriptor = {
@@ -21,6 +26,9 @@
 	    ['b', 'true', 't'],
 	    ['b', 'false', 'f'],
 	    ['b', '%s', 'evaluate', 'false'],
+	    ['b', 'not %s', 'evalnot', 'false'],
+	    ['b', '%s and %s', 'evaland', 'false', 'false'],
+	    ['b', '%s or %s', 'evalor', 'false', 'false'],
         ]
     };
 
