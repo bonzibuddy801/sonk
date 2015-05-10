c5f24695a12b26a783fe8f16212e9e80f563043b
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-05-09 18:18:02 -0600

    even more $%*@*($&#

--- canvasbase.js
+++ canvasbase.js
@@ -32,9 +32,12 @@ window.requestAnimFrame = (function(){
     function generateCanvas()
     {
 	var c = document.createElement("canvas");
-	c.setAttribute("style", "position: absolute; top: 72px; left: 6px; width: 480px; height: 360px; z-index: -1000; background-color: #ffffff;");
+	c.setAttribute("style", "position: absolute; top: 72px; left: 6px; width: 480px; height: 360px; z-index: 1000; background-color: rgba(0, 0, 0, 0);");
 	c.setAttribute("width", "480");
 	c.setAttribute("height", "360");
+	document.body.appendChild(c);
+	var back = document.createElement("div");
+	back.setAttribute("style", "position:absolute; top: 72px; left: 6px; width: 480px; height: 360px; z-index: -1000; background-color: #ffffff;");
 	document.body.appendChild(c);
 	canvas = c;
 	ctx = canvas.getContext("2d");
