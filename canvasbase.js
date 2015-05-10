6c91d6d79e17b2ea04518d1d164c03b2b566baf5
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-05-09 18:41:09 -0600

    nope it didnt and now i dont know how i got it to work

--- canvasbase.js
+++ canvasbase.js
@@ -32,7 +32,7 @@ window.requestAnimFrame = (function(){
     function generateCanvas()
     {
 	var c = document.createElement("canvas");
-	c.setAttribute("style", "position: absolute; top: 72px; left: 6px; width: 480px; height: 360px; z-index: 10; background-color: rgba(0, 0, 0, 0);");
+	c.setAttribute("style", "position: absolute; top: 72px; left: 6px; width: 480px; height: 360px; z-index: 10; background-color: rgba(0, 0, 0, 0); border: solid black 1px;");
 	c.setAttribute("width", "480");
 	c.setAttribute("height", "360");
 	document.body.appendChild(c);
