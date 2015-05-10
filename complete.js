14d26ca45077c65165c37f4a40b71d8f34fa4bb3
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-05-10 11:35:11 -0600

    some things

--- complete.js
+++ complete.js
@@ -1,12 +1,23 @@
 ﻿//MFD Complete: TheMonsterFromTheDeep Complete
 //Uses hax in order to organize multiple extensions under multiple categories
+var loadedMFD = [];
 (function(ext) { ext._shutdown = function() {};
-ext._getStatus = function() { return {status: 1, msg: 'Ready'}; }; //This extension is essentially a blank extension built in order to provide the text seperator
+function getLoadedExtensions()
+{
+	var msgString = "";
+	for(var i = 0; i < loadedMFD.length; i++)
+	{
+		if(loadedMFD[i] != "unloaded") { msgString += loadedMFD[i] + ", "; }
+	}
+	return msgString;
+}
+ext._getStatus = function() { return {status: 1, msg: 'Loaded extensions: ' + getLoadedExtensions()}; }; //This extension is essentially a blank extension built in order to provide the text seperator
 var descriptor = {blocks: []}; ScratchExtensions.register('MFD Complete Extension', descriptor, ext); })({});
 //MFD: Internetting
 //Includes functions for HTTP requests & linking to other projects
 (function(ext) { 
-	ext._shutdown = function() {};
+	var extName = 'MFD: Internetting';
+	ext._shutdown = function() { for(var i = 0; i < loadedMFD.length; i++) { if(loadedMFD[i] == extName) { loadedMFD[i] = "unloaded"; } }};
 	ext._getStatus = function() { return {status: 2, msg: 'Ready'}; };
 	
 	ext.testBlock = function() { return 0; }
@@ -17,5 +28,6 @@ var descriptor = {blocks: []}; ScratchExtensions.register('MFD Complete Extensio
 		]
 	}; 
 
-	ScratchExtensions.register('MFD: Internetting', descriptor, ext); //All the titles begin with "MFD:" in case other extensions with similar names are also used
+	ScratchExtensions.register(extName, descriptor, ext); //All the titles begin with "MFD:" in case other extensions with similar names are also used
+	loadedMFD[loadedMFD.length] = extName; 
 })({});
