eb08b0ec639a7c252e91877e067d52fad85073aa
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-08-16 15:29:40 -0600

    Beginning test of functionality
    some blocks were added that should give me insight on how this project
    might go

--- custom-reporters/script.js
+++ custom-reporters/script.js
@@ -1,4 +1,8 @@
+//themonsterfromthedeep.github.io/scratch-extensions/custom-reporters/script.js
+
 (function(ext) {
+    var reporters = [];
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,9 +12,27 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    ext.addRep = function(name)
+    {
+        reporters[name] = false;
+    }
+
+    ext.hatRep = function(name)
+    {
+        return reporters[name];
+    }
+
+    ext.runRep = function(name)
+    {
+        reporters[name] = true;
+    }
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
+            ['r', 'add reporter %s', 'addRep'],
+            ['h', 'reporter %s', 'hatRep'],
+            ['r', 'run reporter %s', 'runRep'],
         ]
     };
 
